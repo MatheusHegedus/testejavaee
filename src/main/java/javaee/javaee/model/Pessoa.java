b38007e76dacdc2fb7def69d21b33a8749304e9f
Augusto Gernavi <augusto.gernavi@gmail.com>
2019-08-08 11:06:19 -0300

    Adicionando atributos da classe Pessoa

--- src/main/java/javaee/javaee/model/Pessoa.java
+++ src/main/java/javaee/javaee/model/Pessoa.java
@@ -1,5 +1,32 @@
 package javaee.javaee.model;
 
-public class Pessoa {	
+public class Pessoa {
+	private String nome;
+	private String sobrenome;
+	private String profissao;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getProfissao() {
+		return profissao;
+	}
+
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
 
 }
