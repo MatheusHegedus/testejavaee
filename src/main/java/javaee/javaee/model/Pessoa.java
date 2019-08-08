8a6db9fba3caeeb714d8fb5379d6a36455a4eb47
MatheusHegedus <50836913+MatheusHegedus@users.noreply.github.com>
2019-08-08 11:06:36 -0300

    classe pessoa/m

--- src/main/java/javaee/javaee/model/Pessoa.java
+++ src/main/java/javaee/javaee/model/Pessoa.java
@@ -1,5 +1,33 @@
 package javaee.javaee.model;
 
 public class Pessoa {	
-
+	
+	double altura;
+	String email;
+	long cpf;
+	
+	
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getCpf() {
+		return cpf;
+	}
+	public void setCpf(long cpf) {
+		this.cpf = cpf;
+	}
+	@Override
+	public String toString() {
+		return "Pessoa [altura=" + altura + ", email=" + email + ", cpf=" + cpf + "]";
+	}
+	
 }
